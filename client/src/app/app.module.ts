b3f400a99b2765e7209c9e322405a9fa5a078c1f
Божидара Божанова <101105150+BojidaraBojanova@users.noreply.github.com>
2024-03-08 16:36:10 +0200

    Created route for the category page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
+import { RecipesModule } from './recipes/recipes.module';
+import { CategoryComponent } from './recipes/category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    RecipesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
