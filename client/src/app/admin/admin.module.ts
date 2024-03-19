f1e49db15072fb62237e647d3cdad6513871304c
Божидара Божанова <101105150+BojidaraBojanova@users.noreply.github.com>
2024-03-19 17:07:45 +0200

    Added admin navbar component and admin main component

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -4,12 +4,16 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AdminRoutingModule } from "./admin-routing.module";
 import { AdminLoginComponent } from "./login/admin-login.component";
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminNavbarComponent } from './admin-home/admin-navbar/admin-navbar.component';
+import { AdminMainComponent } from './admin-home/admin-main/admin-main.component';
 
 
 @NgModule({
     declarations:[
         AdminLoginComponent,
         AdminHomeComponent,
+        AdminNavbarComponent,
+        AdminMainComponent,
     ],
     imports:[
         CommonModule,
