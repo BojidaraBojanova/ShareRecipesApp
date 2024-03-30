f8ab2d28ddce111d7b71591aec73c9d8c06cb31a
Божидара Божанова <101105150+BojidaraBojanova@users.noreply.github.com>
2024-03-30 16:53:58 +0200

    Added user routes guards

--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -4,27 +4,32 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { UserAuthActivate } from '../guards/userAuth.activate';
 
 const routes: Routes = [
   {
     path: 'users/login',
     component: LoginComponent,
+    canActivate: [!UserAuthActivate]
     
   },
   {
     path: 'users/register',
     component: RegisterComponent,
+    canActivate: [!UserAuthActivate]
+
     
   },
   {
     path: 'users/profile',
     component: ProfileComponent,
+    canActivate: [UserAuthActivate]
     
   },
   {
     path: 'users/addRecipe',
     component: AddRecipeComponent,
-    
+    canActivate: [UserAuthActivate]
   },
 
   
